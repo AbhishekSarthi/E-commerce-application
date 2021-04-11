9d81fa13c8c591d4e125ca5c9241464ce8ea32a9
Abhishek Sarthi <abhi.sarthi15@gmail.com>
2021-04-11 20:34:20 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -6,4 +6,12 @@ app.get('/', (req, res) => {
     res.send('Home Page');
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+} else {
+    app.listen(5000, console.log('Server running on port 5000'));
+}

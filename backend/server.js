768afa8760d5bb17de62d6c3faa29855cf0dc131
Abhishek Sarthi <abhi.sarthi15@gmail.com>
2021-04-11 21:21:38 +0530

    changes3

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.send('Home Page');
-// });
+app.get('/api', (req, res) => {
+    res.send('API Home Page');
+});
 const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, '../client/build')));
@@ -15,14 +15,3 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
-
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '/client/build')));
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
-// else {
-// app.listen(5000, console.log('Server running on port 5000'));
-// }

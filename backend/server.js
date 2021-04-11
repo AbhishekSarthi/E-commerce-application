8e41fe6144ce10d8f6bfd98e42984cd0a72f0e35
Abhishek Sarthi <abhi.sarthi15@gmail.com>
2021-04-11 21:05:37 +0530

    changes1

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Home Page');
-});
-
-app.use(express.static(path.join(__dirname, '../build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'));
-});
-
+// app.get('/', (req, res) => {
+//     res.send('Home Page');
+// });
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build'));
+});
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
@@ -22,6 +22,7 @@ app.listen(port, () => {
 //     app.get('*', (req, res) => {
 //         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 //     });
-// } else {
-//     app.listen(5000, console.log('Server running on port 5000'));
+// }
+// else {
+// app.listen(5000, console.log('Server running on port 5000'));
 // }
